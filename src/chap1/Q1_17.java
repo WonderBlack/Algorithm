d7c1fbdd1b979c65b520f5a5fff5be7a98630f6d
KKaname <kamishimakaname@kamishima-kaname-no-Mac-Pro.local>
2015-02-10 03:58:08 +0900

    コードを追加

--- src/chap1/Q1_17.java
+++ src/chap1/Q1_17.java
@@ -2,7 +2,7 @@ package chap1;
 
 import java.util.Scanner;
 
-public class Q1_16 {
+public class Q1_17 {
 	static Scanner stdIn = new Scanner(System.in);
 	static int n;
 
@@ -17,14 +17,14 @@ public class Q1_16 {
 	static void spira(int n) {
 		int x = 1;// *の個数
 		int y = 0;// スペースの個数
-		int z = n + (n - 1);// 一行あたりのマス目の数
+		int z = (n * 2) - 1;// 一行あたりのマス目の数
 		for (int i = 1; i <= n; i++) {// i = 段数
 			y = (z - x) / 2;// マス目の数から*の数を引いて２等分し左右一方あたりのスペースの数を算出
 			for (int j = 0; j < y; j++) {
 				System.out.print(" ");
 			}
 			for (int j = 0; j < x; j++) {// xの数分*を描画
-				System.out.print("*");
+				System.out.print(i);
 			}
 			x += 2;// 一段上がるごとに２ずつ*の数が増える。
 			System.out.println();
