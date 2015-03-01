0fb2ae7b15554191eda7e20026fedb15c95f18fd
上嶋要 <kaname.k.20130831@etude.ocn.ne.jp>
2015-03-02 04:31:31 +0900

    コードを変更

--- src/chap3/Q3_4.java
+++ src/chap3/Q3_4.java
@@ -20,7 +20,6 @@ public class Q3_4 {
 		}
 		System.out.println("--");
 
-<<<<<<< HEAD
 		for (int i = 0; i < a.length; i++) {
 
 			int pc = (pl + pr) / 2;// 中央要素のインデックス
@@ -60,65 +59,11 @@ public class Q3_4 {
 			// 数字を表示するfor文
 			System.out.printf("%3d", pc);
 			System.out.print("|");
-=======
-		int pc = (pl + pr) / 2;// 中央要素のインデックス
-		System.out.print("   |");
-		// マークを表示するfor文
-		for (int j = 0; j < a.length; j++) {
-			if (j == pl) {
-				System.out.print(" <- ");
-			} else if (j == pr) {
-				System.out.print("  ->");
-			} else if (j == pc) {
-				System.out.print("   +");
-			} else {
-				System.out.print("    ");
-			}
-		}
-		System.out.println();
-		System.out.printf("%3d", pc);
-		System.out.print("|");
-		// 要素を表示するfor文
-		for (int j = 0; j < a.length; j++) {
-			System.out.printf("%4d", a[j]);
-		}
-		System.out.println();
-		if (a[pc] == key) {
-			return pc;// 探索成功
-		} else if (a[pc] < key) {// 中央値よりkwyが大きかったら
-			pl = pc + 1;// 探索範囲を後半に絞り込む
-		} else {// 中央値よりkeyが小さかったら
-			pr = pc - 1;// 探索範囲を前半に絞り込む
-		}
-
-		for (int i = 0; i < a.length; i++) {
-			pc = (pl + pr) / 2;// 中央要素のインデックス
-			System.out.print("   |");
-			// マークを表示するfor文
-			for (int j = 0; j < a.length; j++) {
-				if (j == pl) {
-					System.out.print(" <- ");
-				} else if (j == pr) {
-					System.out.print("  ->");
-				} else if (j == pc) {
-					System.out.print("   +");
-				} else {
-					System.out.print("    ");
-				}
-			}
-			System.out.println();
-			System.out.printf("%3d", pc);
-			System.out.print("|");
-			// 要素を表示するfor文
->>>>>>> origin/master
 			for (int j = 0; j < a.length; j++) {
 				System.out.printf("%4d", a[j]);
 			}
 			System.out.println();
-<<<<<<< HEAD
 
-=======
->>>>>>> origin/master
 			if (a[pc] == key) {
 				return pc;// 探索成功
 			} else if (a[pc] < key) {// 中央値よりkwyが大きかったら
@@ -126,7 +71,6 @@ public class Q3_4 {
 			} else {// 中央値よりkeyが小さかったら
 				pr = pc - 1;// 探索範囲を前半に絞り込む
 			}
-<<<<<<< HEAD
 			
 			if (pl > pr || pr < pl) {
 				break;
@@ -135,12 +79,6 @@ public class Q3_4 {
 
 		// do {
 		// pc = (pl + pr) / 2;// 中央要素のインデックス
-=======
-		}
-
-		// do {
-		// int pc = (pl + pr) / 2;// 中央要素のインデックス
->>>>>>> origin/master
 		// if (a[pc] == key) {
 		// return pc;// 探索成功
 		// } else if (a[pc] < key) {// 中央値よりkwyが大きかったら
@@ -148,10 +86,6 @@ public class Q3_4 {
 		// } else {// 中央値よりkeyが小さかったら
 		// pr = pc - 1;// 探索範囲を前半に絞り込む
 		// }
-<<<<<<< HEAD
-=======
-		//
->>>>>>> origin/master
 		// } while (pl <= pr);
 
 		return -1;// 探索失敗
